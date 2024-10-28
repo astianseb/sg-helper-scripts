dfb96f3b25c717721259ffd1228a50ca2625ca20
astianseb <callmeseb@gmail.com>
2024-10-28 18:37:15 +0100

    index.php source

--- startup.sh
+++ startup.sh
@@ -6,7 +6,7 @@ apt-get install -y wget
 cd /var/www/html
 rm index.html -f
 rm index.php -f
-wget https://storage.googleapis.com/cloud-training/gcpnet/ilb/index.php
+wget https://raw.githubusercontent.com/astianseb/sg-helper-scripts/refs/heads/main/index.php
 META_REGION_STRING=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/zone" -H "Metadata-Flavor: Google")
 REGION=`echo "$META_REGION_STRING" | awk -F/ '{print $4}'`
 sed -i "s|region-here|$REGION|" index.php
