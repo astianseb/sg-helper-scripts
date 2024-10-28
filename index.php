77ebe9899d7cfde315c9ccab147a2883574492d5
astianseb <callmeseb@gmail.com>
2024-10-28 18:33:09 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+$ip = $_SERVER['REMOTE_ADDR'];
+$xff = $_SERVER['X_FORWARDED_FOR']
+
+// display it back
+echo "<h1>Internal Load Balancing Lab</h1>";
+echo "<h2>Client IP</h2>";
+echo "Your IP address : " . $ip;
+echo "X-Forwarded-For : " .$xff;
+echo "<h2>Hostname</h2>";
+echo "Server Hostname: " . php_uname("n");
+echo "<h2>Server Location</h2>";
+echo "Region and Zone: " . "region-here";
+echo "<br>"
+?>
