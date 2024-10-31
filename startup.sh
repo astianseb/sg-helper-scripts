08b63ae297f6f062cff7ad2444de8deec9de6dae
astianseb <callmeseb@gmail.com>
2024-10-31 22:28:24 +0100

    .

--- startup.sh
+++ startup.sh
@@ -7,6 +7,7 @@ cd /var/www/html
 rm index.html -f
 rm index.php -f
 wget https://raw.githubusercontent.com/astianseb/sg-helper-scripts/refs/heads/main/index.php
+wget https://raw.githubusercontent.com/astianseb/sg-helper-scripts/refs/heads/main/server.php
 META_REGION_STRING=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/zone" -H "Metadata-Flavor: Google")
 REGION=`echo "$META_REGION_STRING" | awk -F/ '{print $4}'`
 sed -i "s|region-here|$REGION|" index.php
