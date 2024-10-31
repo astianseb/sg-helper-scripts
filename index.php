4878ef5c216a930b1d709056396a179d7e37caa1
astianseb <callmeseb@gmail.com>
2024-10-31 22:52:29 +0100

    .

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php 
 $ip = $_SERVER['REMOTE_ADDR'];
+$srv_name = $_SERVER['SERVER_NAME'];
 
 // display it back
-echo "Your IP address : " . $ip;
-echo "Server Hostname: " . php_uname("n");
-echo "Region and Zone: " . "region-here";
-echo "$_SERVER variable at <url>/server.php", PHP_EOL; 
+echo "Your IP address: " . $ip " ";
+echo "Server Hostname: " . php_uname("n") " ";
+echo "Region and Zone: " . "region-here" " ";
+echo "\$_SERVER variable at http://{$srv_name}/server.php", PHP_EOL; 
 ?>
