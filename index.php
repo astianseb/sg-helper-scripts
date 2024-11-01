d67af09f2af3520c42a0122c3b62fa5009767953
astianseb <callmeseb@gmail.com>
2024-11-01 07:30:13 +0100

    final

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php 
 $ip = $_SERVER['REMOTE_ADDR'];
 $srv_name = $_SERVER['SERVER_NAME'];
+$load = sys_getloadavg();
+
 
 // display it back
+echo "----------------------------------------------"
 echo "Your IP address: " . $ip . " ";
 echo "Server Hostname: " . php_uname("n") . " ";
+echo "Server load (1/5/15): " . $load[0] . "/" . $load[1] . "/" . $load[2] . " ";
 echo "Region and Zone: " . "region-here" . " ";
-echo "\$_SERVER variable at http://{$srv_name}/server.php", PHP_EOL; 
+echo "----------------------------------------------"
+echo "All \$_SERVER variable at http://{$srv_name}/server.php", PHP_EOL;
+echo ".............................................." 
 ?>
